ee293071741cef478c8b0f31ab359c88f0bdf612
jjongbbang2 <jjongbbang2@naver.com>
2024-09-05 13:05:30 +0900

    init

---
+++ backend-main/catchcatch-main-backend/src/test/java/com/catchcatchmainbackend/CatchcatchMainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchcatchmainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchcatchMainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
