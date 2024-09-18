a3cef26f2d90734fa3b46c92aec02b9ee9f9c8eb
홍경현 <hyeon21owo@gmail.com>
2024-09-19 02:12:03 +0900

    Env: backend-auth 폴더 구조 잡기

---
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/CatchcatchAuthApplication.java
@@ -0,0 +1,13 @@
+package com.catchcatch.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchcatchAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchcatchAuthApplication.class, args);
+    }
+
+}
---
+++ backend-auth/catchcatch-auth/src/test/java/com/catchcatch/auth/CatchcatchAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchcatch.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchcatchAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
