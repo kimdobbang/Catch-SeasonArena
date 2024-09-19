cc2ec335434a5f0e830ffb9c026827bc195ef0d8
홍경현 <hyeon21owo@gmail.com>
2024-09-19 17:29:28 +0900

    Feat: 멤버 저장, 존재하는 멤버 판단하는 포트 impl 구현하는 adapter 생성

---
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/out/persistence/MemberPersistenceAdapter.java
@@ -0,0 +1,30 @@
+package com.catchcatch.auth.domains.member.adapter.out.persistence;
+
+import com.catchcatch.auth.domains.member.application.port.out.ExistsMemberPort;
+import com.catchcatch.auth.domains.member.application.port.out.LoadMemberPort;
+import com.catchcatch.auth.domains.member.application.port.out.SaveMemberPort;
+import com.catchcatch.auth.domains.member.domain.Member;
+import com.catchcatch.auth.global.exception.CustomException;
+import com.catchcatch.auth.global.exception.ExceptionResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class MemberPersistenceAdapter implements SaveMemberPort, ExistsMemberPort {
+
+    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
+
+    @Override
+    public boolean existsByEmailAndIsDeleted(String email, boolean isDeleted) {
+        return memberRepository.existsByEmailAndIsDeleted(email, isDeleted);
+    }
+
+    @Override
+    public void save(Member member) {
+        memberRepository.save(memberMapper.toEntity(member));
+    }
+}
