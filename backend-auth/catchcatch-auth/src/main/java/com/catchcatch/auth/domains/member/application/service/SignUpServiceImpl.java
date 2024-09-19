d6d2c50f4e8191ee027e05ae4025f32126955408
홍경현 <hyeon21owo@gmail.com>
2024-09-19 17:26:03 +0900

    Feat: 회원 가입 Service Impl 구현

---
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/application/service/SignUpServiceImpl.java
@@ -0,0 +1,49 @@
+package com.catchcatch.auth.domains.member.application.service;
+
+import com.catchcatch.auth.domains.member.adapter.in.web.requestdto.SignUpRequestDto;
+import com.catchcatch.auth.domains.member.application.port.in.SignUpUseCase;
+import com.catchcatch.auth.domains.member.application.port.out.ExistsMemberPort;
+import com.catchcatch.auth.domains.member.application.port.out.SaveMemberPort;
+import com.catchcatch.auth.domains.member.domain.Member;
+import com.catchcatch.auth.domains.member.domain.Role;
+import com.catchcatch.auth.global.exception.CustomException;
+import com.catchcatch.auth.global.exception.ExceptionResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class SignUpServiceImpl implements SignUpUseCase {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final SaveMemberPort saveMemberPort;
+    private final ExistsMemberPort existsMemberPort;
+
+    @Override
+    @Transactional
+    public void singUp(SignUpRequestDto requestDto) {
+        if(existsMemberPort.existsByEmailAndIsDeleted(requestDto.email(), false)){
+            log.error("BACK-AUTH:ERROR {}", CustomException.DUPLICATED_EMAIL_EXCEPTION);
+            throw new ExceptionResponse(CustomException.DUPLICATED_EMAIL_EXCEPTION);
+        }
+
+        Member member = Member.createMember(
+                0L,
+                requestDto.email(),
+                passwordEncoder.encode(requestDto.password()),
+                Role.ROLE_USER,
+                requestDto.email(),
+                0,
+                "아바타",
+                false
+        );
+
+        saveMemberPort.save(member);
+    }
+}
