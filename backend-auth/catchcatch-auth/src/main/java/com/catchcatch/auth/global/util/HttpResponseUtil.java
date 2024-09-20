f233cca22a99cd5196eb870ecb614b109aa851d4
홍경현 <hyeon21owo@gmail.com>
2024-09-20 13:09:22 +0900

    Feat: response 응답 수정

--- backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/global/util/HttpResponseUtil.java
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/global/util/HttpResponseUtil.java
@@ -1,5 +1,6 @@
 package com.catchcatch.auth.global.util;
 
+import com.catchcatch.auth.domains.member.adapter.in.web.message.SuccessSignUpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,11 @@ import java.util.Map;
 @Component
 public class HttpResponseUtil {
 
-    public ResponseEntity<Map<String,Object>> createResponse(Object o){
+    public ResponseEntity<Map<String,Object>> createResponse(Object msg, Object code, Object status){
         Map<String, Object> data = new HashMap<>();
-        data.put("data",o);
+        data.put("msg", msg);
+        data.put("code", code);
+        data.put("status", status);
         return ResponseEntity.ok().body(data);
     }
 
