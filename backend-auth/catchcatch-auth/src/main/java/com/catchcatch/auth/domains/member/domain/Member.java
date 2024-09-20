fc729f5868c48b7f6da0d5ce305d824ffb837c15
홍경현 <hyeon21owo@gmail.com>
2024-09-20 13:11:52 +0900

    Feat: Member 도메인 enum type String 변경, 참조형 타입으로 변수 변경, 생성자 인자 MemberEntity (mapper)와 reqestDto로 분리

--- backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/domain/Member.java
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/domain/Member.java
@@ -1,23 +1,23 @@
 package com.catchcatch.auth.domains.member.domain;
 
+import com.catchcatch.auth.domains.member.adapter.in.web.requestdto.SignUpRequestDto;
+import com.catchcatch.auth.domains.member.adapter.out.persistence.MemberEntity;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class Member {
 
-    private Long memberId;
     private String email;
     private String password;
     private Role role;
     private String nickname;
-    private int rating;
+    private Integer rating;
     private String avatar;
-    private boolean isDeleted;
+    private Boolean isDeleted;
 
     @Builder
-    private Member(Long memberId, String email, String password, Role role, String nickname, int rating, String avatar, boolean isDeleted) {
-        this.memberId = memberId;
+    private Member(String email, String password, Role role, String nickname, Integer rating, String avatar, Boolean isDeleted) {
         this.email = email;
         this.password = password;
         this.role = role;
@@ -27,16 +27,27 @@ public class Member {
         this.isDeleted = isDeleted;
     }
 
-    public static Member createMember(Long memberId, String email, String password, Role role, String nickname, int rating, String avatar, boolean isDeleted) {
+    public static Member createMemberMapper(MemberEntity memberEntity) {
         return Member.builder()
-                .memberId(memberId)
-                .email(email)
-                .password(password)
-                .role(role)
-                .nickname(nickname)
-                .rating(rating)
-                .avatar(avatar)
-                .isDeleted(isDeleted)
+                .email(memberEntity.getEmail())
+                .password(memberEntity.getPassword())
+                .role(memberEntity.getRole())
+                .nickname(memberEntity.getNickname())
+                .rating(memberEntity.getRating())
+                .avatar(memberEntity.getAvatar())
+                .isDeleted(memberEntity.getIsDeleted())
+                .build();
+    }
+
+    public static Member createSignUpMember(SignUpRequestDto signUpRequestDto) {
+        return Member.builder()
+                .email(signUpRequestDto.email())
+                .password(signUpRequestDto.password())
+                .role(Role.ROLE_USER)
+                .nickname(signUpRequestDto.email())
+                .rating(0)
+                .avatar("기본 아바타")
+                .isDeleted(false)
                 .build();
     }
 }
