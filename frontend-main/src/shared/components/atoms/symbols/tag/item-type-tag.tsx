c4c7df0ff796cfaf0d326bb3f054111b12ea7f12
고승희 <bookshelf1234@ewhain.net>
2024-09-23 16:45:07 +0900

    Feat: 아이템 타입 태그, NEW 태그, 원형 태그 추가(check SVG 추가)

--- frontend-main/src/shared/components/atoms/symbols/tag/item-type-tag.tsx
+++ frontend-main/src/shared/components/atoms/symbols/tag/item-type-tag.tsx
@@ -1,1 +1,45 @@
-// 희귀도 태그도 똑같이생겼눈데 만들어야함? 나잘모룸
+/* 아이템 타입 표시하는 태그 (무기, 액티브, 패시브) */
+import { itemType } from "@/app/Types/common";
+
+interface ItemTypeTagProps {
+  type?: itemType; // 아이템타입
+  className?: string;
+  color?: "orange" | "gray"; // 색깔
+}
+
+export const ItemTypeTag: React.FC<ItemTypeTagProps> = ({
+  type = "sword",
+  className,
+  color,
+}) => {
+  const getColor = () => {
+    switch (color) {
+      case "orange":
+        return "bg-catch-sub-400 text-catch-gray-000";
+      case "gray":
+        return "bg-catch-gray-200 text-catch-gray-300";
+      default:
+        return "bg-catch-sub-400 text-catch-gray-000";
+    }
+  };
+
+  const getType = () => {
+    switch (type) {
+      case "sword":
+        return "무기";
+      case "active":
+        return "액티브";
+      case "passive":
+        return "패시브";
+      default:
+        return "무기";
+    }
+  };
+  return (
+    <div
+      className={`w-[60px] h-[24px] bg-catch-sub-400 shrink-0 text-center rounded-full ${className} ${getColor()}`}
+    >
+      <p className="text-body2 text-white">{getType()}</p>
+    </div>
+  );
+};
