f0a23d289e697f4a8343bf9362de4b7fd75b711b
kimdoi <kimdoiary@gmail.com>
2024-09-24 07:10:54 +0900

    "Refactor: serviceTitle 수정"

--- frontend-main/src/shared/ui/layout/service-title.tsx
+++ frontend-main/src/shared/ui/layout/service-title.tsx
@@ -5,7 +5,7 @@ export const ServiceTitle: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-row mr-24">
-        <Leave width="50" className=" fill-catch-main-7 00" />
+        <Leave width="50" className=" fill-catch-main-700" />
         <Leave color="text-catch-main-700" className="rotate-90" />
       </div>
       <ServiceTitleSymbol />
