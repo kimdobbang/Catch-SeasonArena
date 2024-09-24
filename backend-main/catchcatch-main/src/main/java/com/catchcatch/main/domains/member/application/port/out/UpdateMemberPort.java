3c0eba6081779c2cf9821b90f7fc0fbf21e2a0ec
홍경현 <hyeon21owo@gmail.com>
2024-09-24 17:35:27 +0900

    Feat: 멤버 수정하는 out port

---
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/member/application/port/out/UpdateMemberPort.java
@@ -0,0 +1,8 @@
+package com.catchcatch.main.domains.member.application.port.out;
+
+import com.catchcatch.main.domains.member.domain.Member;
+
+public interface UpdateMemberPort {
+
+    void updateMember(Member member);
+}
