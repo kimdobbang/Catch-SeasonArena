2e1b311c8def9c9f0628ccc631b5b9a15c7d7306
홍경현 <hyeon21owo@gmail.com>
2024-09-24 17:37:38 +0900

    Feat: 멤버 아이디로 멤버 엔티티 찾는 repository

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/member/adapter/out/persistence/MemberEntityRepository.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/member/adapter/out/persistence/MemberEntityRepository.java
@@ -2,7 +2,10 @@ package com.catchcatch.main.domains.member.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberEntityRepository extends JpaRepository<MemberEntity, Long> {
 
     Boolean existsByNicknameAndIsDeleted(String nickname, Boolean isDeleted);
+    Optional<MemberEntity> findByMemberIdAndIsDeleted(Long memberId, Boolean isDeleted);
 }
