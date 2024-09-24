01290773357b111af11f7f3704c3d654555f5104
jjongbbang2 <jjongbbang2@naver.com>
2024-09-24 23:53:20 +0900

    feat : Rank-server redis application 샤딩으로 수정

--- backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/global/redis/config/RedisConfig.java
+++ backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/global/redis/config/RedisConfig.java
@@ -1,38 +1,34 @@
 package com.catchcatchrank.global.redis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
 	@Bean
-	public RedisTemplate<String, Object> redisTemplateBronze() {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory("3.36.122.163", 7001)); // Bronze 노드
-		return template;
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(host, port);
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> redisTemplateSilver() {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory("3.36.122.163", 7002)); // Silver 노드
+	public RedisTemplate<?, ?> redisTemplate() {
+		RedisTemplate<?, ?> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
+		template.setEnableTransactionSupport(true);
 		return template;
-	}
-
-	@Bean
-	public RedisTemplate<String, Object> redisTemplateGold() {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory("3.36.122.163", 7003)); // Gold 노드
-		return template;
-	}
-
-	private RedisConnectionFactory redisConnectionFactory(String host, int port) {
-		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
-		return new LettuceConnectionFactory(config);
 	}
 }
