dc35fe5807b43f463366acc5ef27236073e3b29f
홍경현 <hyeon21owo@gmail.com>
2024-09-24 17:38:43 +0900

    Feat: 정보 은닉 권한 수정 및 존재하지 않는 멤버 예외 추가

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/exception/CustomException.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/exception/CustomException.java
@@ -1,13 +1,15 @@
 package com.catchcatch.main.global.exception;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum CustomException {
 
-    DUPLICATED_NICKNAME_EXCEPTION(400, "DuplicatedNicknameException", "존재하는 닉네임입니다.");
+    DUPLICATED_NICKNAME_EXCEPTION(400, "DuplicatedNicknameException", "존재하는 닉네임입니다."),
+    NOT_EXISTS_MEMBER_EXCEPTION(400, "NotExistsMemberException", "존재하지 않는 멤버입니다.");
 
     private Integer statusNum;
     private String errorCode;
