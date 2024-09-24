4b046260f0f6c5ef33b3b7aca5883ba7e2c24c4b
홍경현 <hyeon21owo@gmail.com>
2024-09-24 14:49:16 +0900

    Refactor: 생성자 정보 은닉 (protected)

--- backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/in/web/message/SuccessCheckEmailMessage.java
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/in/web/message/SuccessCheckEmailMessage.java
@@ -1,10 +1,11 @@
 package com.catchcatch.auth.domains.member.adapter.in.web.message;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum SuccessCheckEmailMessage {
 
     SUCCESS_CHECK_EMAIL("가입되지 않은 이메일입니다.");
--- backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/in/web/message/SuccessSignUpMessage.java
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/in/web/message/SuccessSignUpMessage.java
@@ -1,10 +1,11 @@
 package com.catchcatch.auth.domains.member.adapter.in.web.message;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public enum SuccessSignUpMessage {
 
     SUCCESS_SIGN_UP("회원가입을 완료했습니다.");
