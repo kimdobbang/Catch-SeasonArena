797fc9bc2c7c4733f15659deaeeb6c6e97697078
jjongbbang2 <jjongbbang2@naver.com>
2024-09-24 15:11:33 +0900

    init

---
+++ backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/CatchcatchRankApplication.java
@@ -0,0 +1,13 @@
+package com.catchcatchrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchcatchRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchcatchRankApplication.class, args);
+	}
+
+}
