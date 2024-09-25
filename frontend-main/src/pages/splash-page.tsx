17ca8238edb68881dbb06226d6d5f967aeaf8307
kimdoi <kimdoiary@gmail.com>
2024-09-25 10:17:12 +0900

    FE/Refactor-스플레시 타이틀 위치 변경

--- frontend-main/src/pages/splash-page.tsx
+++ frontend-main/src/pages/splash-page.tsx
@@ -2,7 +2,9 @@ import { ServiceTitle } from "@/shared/ui";
 export const SplashPage = () => {
   return (
     <div className="flex items-center justify-center w-full h-full bg-catch-sub-100">
-      <ServiceTitle />
+      <div className="transform -translate-y-12">
+        <ServiceTitle />
+      </div>
     </div>
   );
 };
