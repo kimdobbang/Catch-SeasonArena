dffb263a75bcf4e5ba8552d3272a1c1791358865
kimdoi <kimdoiary@gmail.com>
2024-09-26 02:36:52 +0900

    FE/Feat:auth 토큰 재발급 요청 API추가

--- frontend-main/src/pages/root-page.tsx
+++ frontend-main/src/pages/root-page.tsx
@@ -1,3 +1,4 @@
+import config from "@/config";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -8,47 +9,37 @@ export const RootPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    console.log("0. 로그인 후 리디렉션 성공");
+  console.log("0. 로그인 후 리디렉션 성공");
+  const fetchTokenFromHeader = async () => {
+    try {
+      const response = await fetch(`${config.API_BASE_URL}/api/auth/reissue`, {
+        method: "GET",
+        credentials: "include", //쿠키포함
+      });
 
-    // 리디렉션 후 헤더에서 토큰 가져오는 함수
-    const fetchTokenFromHeader = async () => {
-      try {
-        const response = await fetch(window.location.href, {
-          method: "GET",
-          credentials: "include", // 쿠키 포함 요청
-          redirect: "manual", // 리다이렉션을 따르지 않음
-        });
+      console.log("1. response: ", response);
 
-        console.log("1. response: ", response);
+      // Authorization 헤더에서 토큰 추출
+      const authorizationHeader = response.headers.get("Authorization");
+      console.log("2. authorizationHeader: ", authorizationHeader);
 
-        // Authorization 헤더에서 토큰 추출 (Bearer <token> 형식)
-        const authorizationHeader = response.headers.get("Authorization");
-        console.log("2. authorizationHeader: ", authorizationHeader);
+      if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
+        const accessToken = authorizationHeader.replace("Bearer ", "");
 
-        if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
-          // "Bearer " 접두사를 제거하고 순수한 토큰 추출
-          const accessToken = authorizationHeader.replace("Bearer ", "");
-
-          // 토큰을 로컬 스토리지에 저장
-          localStorage.setItem("token", accessToken);
-          console.log("3. OAuth 로그인 성공, 엑세스 토큰:", accessToken);
-
-          // Redux에 토큰 저장
-          dispatch(setAuth({ accessToken }));
-
-          // 메인 페이지로 이동
-          navigate("/main");
-        } else {
-          throw new Error("Authorization 헤더에 유효한 토큰이 없습니다.");
-        }
-      } catch (error) {
-        console.error("OAuth 로그인 실패:", error);
-        navigate("/login");
+        // 토큰을 로컬 스토리지에 저장
+        localStorage.setItem("token", accessToken);
+        console.log("3. OAuth 로그인 성공, 토큰 저장 완:", accessToken);
+        dispatch(setAuth({ accessToken }));
+        navigate("/main");
+      } else {
+        throw new Error("Authorization 헤더에 유효한 토큰이 없습니다.");
       }
-    };
-
-    // 리디렉션 후 토큰을 가져오기 위해 함수 호출
+    } catch (error) {
+      console.error("OAuth 로그인 실패:", error);
+      navigate("/login");
+    }
+  };
+  useEffect(() => {
     fetchTokenFromHeader();
   }, [dispatch, navigate]);
 
