d2de713d105274e0ca18613fff318ba8e42fad1e
kimdoi <kimdoiary@gmail.com>
2024-09-26 17:45:29 +0900

    FE/인벤토리 유저정보 컴포넌트

--- frontend-main/src/shared/components/entities/inventory/inventory-userinfo.tsx
+++ frontend-main/src/shared/components/entities/inventory/inventory-userinfo.tsx
@@ -1,19 +1,37 @@
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
+import { getTierByRating } from "@/app/types/tier";
+import { InGameStats } from "@entities/index"; // InGameStats 컴포넌트 가져오기
+
 export const InventoryUserInfo = () => {
+  // Redux 상태에서 유저 정보 가져오기
+  const { selectedAvatar, rating } = useSelector(
+    (state: RootState) => state.user,
+  );
+
+  // 유저의 티어 계산
+  const userTier = getTierByRating(rating);
+
   return (
     <div className="flex items-center justify-around h-[40%]">
-      <div className="font-pretendard text-sub2">유저아바타</div>
-      <div className="ml-9">
-        <div className="font-pretendard text-sub2">유저정보</div>
-        <div className="flex">
-          <div>티어 뱃지</div>
-          <div>티어 text</div>
+      {/* 아바타 */}
+      <div className="w-10">
+        <div>선택아바타: {selectedAvatar}</div>
+      </div>
+
+      <div className="w-full ml-9">
+        {/* 유저 티어 정보 */}
+        <div className="mb-4 text-lg text-gray-700 font-pretendard">
+          티어 뱃지
         </div>
-        <div className="flex-col">
-          <div className="font-pretendard text-sub2">인게임 스탯</div>
-          <div>체력 스탯 기본100 스탯</div>
-          <div>공격력 스탯 기본10</div>
-          <div>속도 스탯 기본10</div>
+        <div className="flex items-center mb-4">
+          <div className={`text-xl font-semibold text-catch-tier-${userTier}`}>
+            {userTier}
+          </div>
         </div>
+
+        {/* Redux로부터 상태를 직접 가져오는 InGameStats */}
+        <InGameStats />
       </div>
     </div>
   );
