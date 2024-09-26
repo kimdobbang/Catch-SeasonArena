19160f8ceeba8a4f83d0fe8a56aa4d8d4be4dbd2
jjongbbang2 <jjongbbang2@naver.com>
2024-09-27 00:32:45 +0900

    feat : Rank-server - rank update 포트

---
+++ backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/domains/rank/application/port/out/UpdateTierPort.java
@@ -0,0 +1,8 @@
+package com.catchcatchrank.domains.rank.application.port.out;
+
+import com.catchcatchrank.domains.rank.domain.Rank;
+
+public interface UpdateTierPort {
+
+	void updateRank(Rank rank);
+}
