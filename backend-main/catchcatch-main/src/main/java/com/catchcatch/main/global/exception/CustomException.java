5bce33907221e76b35fab64d823a7187b0f461c0
jjongbbang2 <jjongbbang2@naver.com>
2024-09-29 23:40:52 +0900

    BE-MAIN/feature - 인벤토리 존재하지 않을시 예외 추가

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/exception/CustomException.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/exception/CustomException.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 public enum CustomException {
 
     DUPLICATED_NICKNAME_EXCEPTION(400, "DuplicatedNicknameException", "존재하는 닉네임입니다."),
+    NOT_EXISTS_INVENTORY_EXCEPTION(400, "NotExistsInventoryException", "존재하지 않는 인벤토리 입니다."),
     NOT_EXISTS_MEMBER_EXCEPTION(400, "NotExistsMemberException", "존재하지 않는 멤버입니다.");
 
     private Integer statusNum;
