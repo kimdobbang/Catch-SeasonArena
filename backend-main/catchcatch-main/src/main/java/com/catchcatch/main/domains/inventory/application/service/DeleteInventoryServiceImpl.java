f5b6dd9c15a8f91753f8d8b722ddc7ece24261a8
jjongbbang2 <jjongbbang2@naver.com>
2024-09-29 23:40:19 +0900

    BE-MAIN/feature - 인벤토리 삭제 서비스

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/application/service/DeleteInventoryServiceImpl.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/application/service/DeleteInventoryServiceImpl.java
@@ -3,7 +3,11 @@ package com.catchcatch.main.domains.inventory.application.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.catchcatch.main.domains.inventory.adapter.out.persistence.InventoryEntity;
 import com.catchcatch.main.domains.inventory.application.port.in.DeleteInventoryUseCase;
+import com.catchcatch.main.domains.inventory.application.port.out.DeleteInventoryPort;
+import com.catchcatch.main.domains.inventory.application.port.out.FindInventoryByIdAndMemberEmail;
+import com.catchcatch.main.domains.inventory.domain.Inventory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +16,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DeleteInventoryServiceImpl implements DeleteInventoryUseCase {
 
-	@Override
-	public void deleteInventory(Long InventoryId, String userEmail) {
+	private final DeleteInventoryPort deleteInventoryPort;
+	private final FindInventoryByIdAndMemberEmail findInventoryByIdAndMemberEmail;
 
+
+	@Override
+	public void deleteInventory(Long inventoryId, String userEmail) {
+		InventoryEntity inventoryEntity = findInventoryByIdAndMemberEmail.findInventoryByIdAndMemberEmail(inventoryId, userEmail);
+		Inventory inventory = Inventory.createInventory(inventoryEntity);
+		deleteInventoryPort.deleteInventory(inventory);
 	}
 }
