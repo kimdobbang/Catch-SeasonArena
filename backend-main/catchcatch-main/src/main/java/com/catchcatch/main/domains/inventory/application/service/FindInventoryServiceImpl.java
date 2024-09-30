7600696d3207b4748a6428fcded5e9c7ec5ba94c
jjongbbang2 <jjongbbang2@naver.com>
2024-09-30 13:02:25 +0900

    BE-MAIN/feature - findInventoriesService

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/application/service/FindInventoryServiceImpl.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/application/service/FindInventoryServiceImpl.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.catchcatch.main.domains.inventory.adapter.out.persistence.InventoryEntity;
 import com.catchcatch.main.domains.inventory.application.port.in.FindInventoriesUseCase;
+import com.catchcatch.main.domains.inventory.application.port.out.FindInventoriesByEmailPort;
 import com.catchcatch.main.domains.inventory.domain.Inventory;
 
 import lombok.RequiredArgsConstructor;
@@ -16,9 +18,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FindInventoryServiceImpl implements FindInventoriesUseCase {
 
+	private final FindInventoriesByEmailPort findInventoriesByEmailPort;
+
 	@Override
 	public List<Inventory> findInventories(String userEmail) {
+		List<InventoryEntity> inventoryEntities = findInventoriesByEmailPort.findInventoriesByEmail(userEmail);
 		List<Inventory> inventories = new ArrayList<>();
+		for(InventoryEntity inventoryEntity : inventoryEntities) {
+			Inventory inventory = Inventory.createInventory(inventoryEntity);
+			inventories.add(inventory);
+		}
 		return inventories;
 	}
 }
