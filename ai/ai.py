3feb7af9a49ca1cb080f6b2b9f278079721f9e3b
JOON-KR <yyj4147@gmail.com>
2024-09-30 15:57:36 +0900

    fix: cors설정 local 추가

--- ai/ai.py
+++ ai/ai.py
@@ -4,15 +4,30 @@ from ultralytics import YOLO
 from PIL import Image
 import io
 import requests  # REST 요청을 보내기 위해 사용
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정
+origins = [
+    "http://localhost:3000",  # 허용할 도메인 (예: 로컬에서 프론트엔드가 실행될 때)
+    "https://j11b106.p.ssafy.io"  # 특정 도메인만 허용
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 허용할 Origin 리스트
+    allow_credentials=True,  # 쿠키와 인증 정보를 포함한 요청 허용
+    allow_methods=["POST"],  # POST 메서드만 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
 
 # YOLOv8 모델 로드
 model = YOLO('./model.pt')  # YOLOv8 모델 로드
 
 # 클래스 이름과 itemId의 매핑
 class_name_to_item_id = {
-    'squirrel': 0,
+    'squirrel': 10,
     'dragonfly': 4,
     'maple': 1,
     'pumpkins': 2,
@@ -48,8 +63,8 @@ async def detect_objects(
                     class_name = model.names[int(cls)]
                     if class_name in class_name_to_item_id:
                         highest_confidence = conf
-                        detect_result = {
-                            "file_name": file.filename,
+                        detectResult = {
+                            "fileName": file.filename,
                             "xmin": int(box[0]),
                             "ymin": int(box[1]),
                             "xmax": int(box[2]),
@@ -59,16 +74,16 @@ async def detect_objects(
                         }
 
     # 신뢰도 0.8 이상인 탐지 결과가 있을 때만 전송
-    if detect_result:
+    if detectResult:
         payload = {
             "email": email,  # 이메일 추가
-            "detectResult": detect_result  # 탐지 결과 추가
+            "detectResult": detectResult  # 탐지 결과 추가
         }
 
         # REST 요청을 보내기
         response = requests.post(api_url, json=payload)
 
-        # API 응답 반환
-        return {"status": "success", "response": response.json()}
+    if response.status_code == 200:
+        return {"status": "success"}
     else:
-        return {"message": "No detection with confidence >= 0.8"}
+        return {"status": "failure"}
