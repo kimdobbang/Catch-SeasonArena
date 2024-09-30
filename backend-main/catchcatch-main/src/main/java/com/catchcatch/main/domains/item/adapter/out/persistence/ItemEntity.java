76eff515f9134290d82289c2fd8cda105ab7d87c
jjongbbang2 <jjongbbang2@naver.com>
2024-09-30 13:03:53 +0900

    BE-MAIN/feature - ItemEntity 전체 생성자 추가

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/item/adapter/out/persistence/ItemEntity.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/item/adapter/out/persistence/ItemEntity.java
@@ -9,11 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 @Table(name = "item")
 public class ItemEntity {
