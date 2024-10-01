6f18e626f2350c0f0181d273083a7eb29fdac349
jjongbbang2 <jjongbbang2@naver.com>
2024-10-01 15:40:22 +0900

    BE-MAIN/feature - 인벤토리 이미 장착시 exception 터트리기

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/exception/CustomException.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/exception/CustomException.java
@@ -10,6 +10,7 @@ public enum CustomException {
 
     DUPLICATED_NICKNAME_EXCEPTION(400, "DuplicatedNicknameException", "존재하는 닉네임입니다."),
     NOT_EXISTS_INVENTORY_EXCEPTION(400, "NotExistsInventoryException", "존재하지 않는 인벤토리 입니다."),
+    INVENTORY_ALREADY_UN_EQUIPPED_EXCEPTION(400, "InventoryAlreadyUnequippedException", "이미 장착 해제된 인벤토리 입니다."),
     NOT_EXISTS_MEMBER_EXCEPTION(400, "NotExistsMemberException", "존재하지 않는 멤버입니다.");
 
     private Integer statusNum;
