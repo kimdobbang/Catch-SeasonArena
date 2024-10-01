d4c66abc2354ed708aa8f81d30b78d6c0448a9a1
limjonghyeok <jjognbbang2@naver.com>
2024-10-01 23:01:29 +0900

    "BE/AUTH - feature 장비 장착 main 연동"

---
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/inventory/adapter/out/EquipInventoryClient.java
@@ -0,0 +1,15 @@
+package com.catchcatch.auth.domains.inventory.adapter.out;
+
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@FeignClient(name = "EquipInventoryClient", url = "https://j11b106.p.ssafy.io/api/main/inventories/items/equipment")
+public interface EquipInventoryClient {
+
+	@PutMapping(value = "/{inventory_id}/member/{member_email}", consumes = "application/json")
+	ResponseEntity<Map<String, Object>> equipInventory(@PathVariable("inventory_id") Long inventoryId, @PathVariable("member_email") String memberEmail);
+}
