f6505cb1e6ebbe3ded804243792770decc04db7a
jjongbbang2 <jjongbbang2@naver.com>
2024-10-02 20:55:14 +0900

    "BE-MAIN : fix n+1 문제 해결"

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/adapter/out/persistence/InventoryRepository.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/adapter/out/persistence/InventoryRepository.java
@@ -3,6 +3,7 @@ package com.catchcatch.main.domains.inventory.adapter.out.persistence;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ public interface InventoryRepository extends JpaRepository<InventoryEntity, Long
 
 	Optional<InventoryEntity> findByIdAndMember_Email(Long id, String email);
 	List<InventoryEntity> findAllByMember_Email(String email);
+	@EntityGraph(attributePaths = {"item"})
 	List<InventoryEntity> findAllByMember_EmailAndIsEquipped(String email, boolean isEquipped);
 }
