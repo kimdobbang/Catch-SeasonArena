829faacbeb443b11e75322e038a3edc50492f673
kimdoi <kimdoiary@gmail.com>
2024-10-02 10:57:24 +0900

    FE/인벤토리 api 생성

--- frontend-main/src/app/apis/inventoryApi.ts
+++ frontend-main/src/app/apis/inventoryApi.ts
@@ -1,4 +1,12 @@
-// 책임 분리
-// 각 컴포넌트: 상태관리, 스토리지 관리, API 처리 책임
+// src/app/api/inventoryApi.ts
 // Api: API 호출과 서버 통신 책임, 데이터 통신(여기서 상태관리x)
 // import config from "@/config";
+
+export const fetchItems = async () => {
+  const response = await fetch("/api/auth/inventories/items");
+  if (!response.ok) {
+    throw new Error("Failed to fetch items");
+  }
+
+  return response.json(); // 데이터 반환
+};
