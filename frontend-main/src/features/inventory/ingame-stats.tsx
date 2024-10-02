6dba80d438ba6d5021cbe161ab9ff0789992a946
kimdoi <kimdoiary@gmail.com>
2024-10-02 18:52:37 +0900

    FE/inventory 유저스탯 수정

--- frontend-main/src/features/inventory/ingame-stats.tsx
+++ frontend-main/src/features/inventory/ingame-stats.tsx
@@ -7,11 +7,10 @@ export const InGameStats = () => {
   const currentStats = stats;
 
   return (
-    <div className="flex-col space-y-2">
-      {/* 체력 */}
+    <div className="flex-col mb-3">
       <div className="flex items-center">
-        <div className="w-20 text-sm text-gray-600">체력</div>
-        <div className="w-full bg-gray-200 rounded-full h-2.5 mx-2">
+        <div className="text-sm text-gray-600 w-14">체력</div>
+        <div className="w-full bg-gray-200 rounded-full h-2.5">
           <div
             className="bg-catch-main-400 h-2.5 rounded-full"
             style={{ width: `${(currentStats.hp / 200) * 100}%` }}
@@ -20,10 +19,9 @@ export const InGameStats = () => {
         <p className="w-8 text-right">{currentStats.hp}</p>
       </div>
 
-      {/* 사거리 */}
       <div className="flex items-center">
-        <div className="w-20 text-sm text-gray-600">사거리</div>
-        <div className="w-full bg-gray-200 rounded-full h-2.5 mx-2">
+        <div className="text-sm text-gray-600 w-14">사거리</div>
+        <div className="w-full bg-gray-200 rounded-full h-2.5">
           <div
             className="bg-catch-main-400 h-2.5 rounded-full"
             style={{ width: `${(currentStats.coverage / 20) * 100}%` }}
@@ -32,10 +30,9 @@ export const InGameStats = () => {
         <p className="w-8 text-right">{currentStats.coverage}</p>
       </div>
 
-      {/* 속도 */}
       <div className="flex items-center">
-        <div className="w-20 text-sm text-gray-600">속도</div>
-        <div className="w-full bg-gray-200 rounded-full h-2.5 mx-2">
+        <div className="text-sm text-gray-600 w-14">속도</div>
+        <div className="w-full bg-gray-200 rounded-full h-2.5">
           <div
             className="bg-catch-main-400 h-2.5 rounded-full"
             style={{ width: `${(currentStats.speed / 20) * 100}%` }}
