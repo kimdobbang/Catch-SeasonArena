b434b64d28dc512846da84d7db11f7b166e4c257
홍경현 <hyeon21owo@gmail.com>
2024-10-02 10:07:42 +0900

    Refactor: 장착 인벤토리 외부 포트 반환값 수정 (엔티티 -> 도메인)

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/application/port/out/FindEquipInventoryByEmailPort.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/application/port/out/FindEquipInventoryByEmailPort.java
@@ -1,10 +1,10 @@
 package com.catchcatch.main.domains.inventory.application.port.out;
 
-import com.catchcatch.main.domains.inventory.adapter.out.persistence.InventoryEntity;
+import com.catchcatch.main.domains.inventory.domain.Inventory;
 
 import java.util.List;
 
 public interface FindEquipInventoryByEmailPort {
 
-    List<InventoryEntity> findEquipInventoryByEmail(String email);
+    List<Inventory> findEquipInventoryByEmail(String email);
 }
