7887f974fe3d4c758d0c09e696ab3ed34597f584
홍경현 <hyeon21owo@gmail.com>
2024-10-03 16:00:16 +0900

    Feat: cors 설정

---
+++ backend-recommend/catchcatch-recommend/src/main/java/catchcatch/recommend/global/config/CorsMvcConfig.java
@@ -0,0 +1,16 @@
+package catchcatch.recommend.global.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .exposedHeaders("Authorization", "Set-Cookie")
+                .allowedOrigins("https://j11b106.p.ssafy.io", "http://localhost:3000", "http://127.0.0.1:5500/")
+                .allowCredentials(true);
+    }
+}
