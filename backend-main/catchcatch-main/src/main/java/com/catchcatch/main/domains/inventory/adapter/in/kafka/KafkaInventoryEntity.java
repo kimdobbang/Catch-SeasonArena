1b5f4f792f10a575f131d2db180d79e124874009
jjongbbang2 <jjongbbang2@naver.com>
2024-10-03 14:24:26 +0900

    "BE-MAIN : refactor - 카프카 설정"

--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/adapter/in/kafka/KafkaInventoryEntity.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/inventory/adapter/in/kafka/KafkaInventoryEntity.java
@@ -4,10 +4,12 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class KafkaInventoryEntity {
 	private String nickname;
 }
--- backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/kafka/consumer/KafkaConsumer.java
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/global/kafka/consumer/KafkaConsumer.java
@@ -1,9 +1,11 @@
 package com.catchcatch.main.global.kafka.consumer;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.catchcatch.main.domains.inventory.adapter.in.kafka.KafkaInventoryEntity;
+import com.catchcatch.main.domains.inventory.adapter.in.kafka.KafkaSaveInventoryEntity;
 import com.catchcatch.main.domains.inventory.application.port.in.DecreaseDurabilityUseCase;
 import com.catchcatch.main.domains.member.domain.Member;
 
@@ -16,11 +18,18 @@ import lombok.extern.slf4j.Slf4j;
 public class KafkaConsumer {
 
 	private final DecreaseDurabilityUseCase decreaseDurabilityUseCase;
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	@KafkaListener(topics = "game_result")
 	public void listen(KafkaInventoryEntity inventoryEntity) {
-		log.info("BE-MAIN : 게임 끝날시 컨슘 {}" , inventoryEntity.getNickname());
+		log.info("BE-MAIN : 게임 끝날시 컨슘 {}" , inventoryEntity.toString());
 		decreaseDurabilityUseCase.decreaseDurability(inventoryEntity.getNickname());
 
+	}
+
+	@KafkaListener(topics = "create_inventory", containerFactory = "kafkaSaveInventoryListenerContainerFactory")
+	public void listen(KafkaSaveInventoryEntity kafkaSaveInventoryEntity) {
+		log.info("BE-MAIN : saveEntity 커슘 {}" , kafkaSaveInventoryEntity.toString());
+		applicationEventPublisher.publishEvent(kafkaSaveInventoryEntity);
 	}
 }
