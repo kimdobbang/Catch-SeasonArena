e05ae04696b74ecb425fe79a7db9d301ff59615d
홍경현 <hyeon21owo@gmail.com>
2024-10-03 15:57:54 +0900

    Feat: 5초에 한번씩 매칭 실행

---
+++ backend-recommend/catchcatch-recommend/src/main/java/catchcatch/recommend/global/config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package catchcatch.recommend.global.config;
+
+import catchcatch.recommend.domain.recommend.service.RecommendService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SchedulerConfig {
+
+    private final RecommendService recommendService;
+
+    @Scheduled(fixedDelay = 5000)
+    public void run(){
+        recommendService.matchingGame();
+    }
+}
