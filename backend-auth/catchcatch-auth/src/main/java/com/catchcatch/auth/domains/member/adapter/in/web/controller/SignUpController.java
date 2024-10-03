0b2e606d7f4fa2cab96c393ac955af07788c4c34
홍경현 <hyeon21owo@gmail.com>
2024-10-03 17:42:21 +0900

    Refactor: 회원가입 시 액세스 토큰 생성 후 헤더에 담기 (컨트롤러 수정)

--- backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/in/web/controller/SignUpController.java
+++ backend-auth/catchcatch-auth/src/main/java/com/catchcatch/auth/domains/member/adapter/in/web/controller/SignUpController.java
@@ -5,6 +5,7 @@ import com.catchcatch.auth.domains.member.adapter.in.web.requestdto.SignUpReques
 import com.catchcatch.auth.domains.member.application.port.in.SignUpUseCase;
 import com.catchcatch.auth.global.util.HttpResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,17 +26,20 @@ public class SignUpController {
 
     @Operation(summary = "회원가입", description = "이메일, 비밀번호로 회원가입")
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDto requestDto){
-        signUpUseCase.singUp(requestDto);
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDto requestDto, HttpServletResponse response){
+        String accessToken = signUpUseCase.singUp(requestDto);
 
-        ResponseEntity<?> response = responseUtil.createResponse(
+        response.addHeader("Authorization", "Bearer " + accessToken);
+        response.setContentType("application/json;charset=UTF-8");
+
+        ResponseEntity<?> responseEntity = responseUtil.createResponse(
                 SuccessSignUpMessage.SUCCESS_SIGN_UP.getMessage(),
                 SuccessSignUpMessage.SUCCESS_SIGN_UP,
                 200);
 
         log.info("BACK-AUTH:SIGN-UP request: {}", requestDto);
-        log.info("BACK-AUTH:SIGN-UP response: {}", response);
+        log.info("BACK-AUTH:SIGN-UP response: {}", responseEntity);
 
-        return response;
+        return responseEntity;
     }
 }
