8bf5e13438980301438b13db308956a3c33df4c4
jjongbbang2 <jjongbbang2@naver.com>
2024-10-03 14:27:56 +0900

    BE/RANK - refactor 비동기 설정

--- backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/CatchcatchRankApplication.java
+++ backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/CatchcatchRankApplication.java
@@ -2,8 +2,10 @@ package com.catchcatchrank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CatchcatchRankApplication {
 
 	public static void main(String[] args) {
