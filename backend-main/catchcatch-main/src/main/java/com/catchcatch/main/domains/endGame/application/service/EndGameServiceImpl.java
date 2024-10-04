a3d6ff78e4366d1b090841b5e10cdd0f4b96fd99
jjongbbang2 <jjongbbang2@naver.com>
2024-10-04 17:01:00 +0900

    "BE-MAIN : feature endgame service"

---
+++ backend-main/catchcatch-main/src/main/java/com/catchcatch/main/domains/endGame/application/service/EndGameServiceImpl.java
@@ -0,0 +1,37 @@
+package com.catchcatch.main.domains.endGame.application.service;
+
+import org.springframework.stereotype.Service;
+
+import com.catchcatch.main.domains.endGame.application.port.in.EndGameUseCase;
+import com.catchcatch.main.domains.endGame.application.port.out.EndGamePort;
+import com.catchcatch.main.domains.endGame.domain.EndGame;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EndGameServiceImpl implements EndGameUseCase {
+
+	private final EndGamePort endGamePort;
+
+	@Override
+	public EndGame getEndGame(String userNickname) {
+		EndGame endGame = endGamePort.getEndGame(userNickname);
+		Integer score = calculateScore(endGame.getKill(), endGame.getTime(),
+			endGame.getRank());
+		Integer preRate = endGame.getRating();
+		Integer calculateRate = preRate + score;
+
+		endGame.resultRating(calculateRate);
+		return endGame;
+	}
+
+	private Integer calculateScore(Integer kill, Integer time, Integer rank) {
+		Integer score = 0;
+
+		score += kill * 100;
+		score += 5 - (rank - 1);
+
+		return score;
+	}
+}
