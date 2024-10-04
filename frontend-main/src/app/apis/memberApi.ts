357b72e5e60df9469fbe1374a55c6ec3d71dae4e
kimdoi <kimdoiary@gmail.com>
2024-10-04 13:59:43 +0900

    FE/Feat- 유저 닉네임 중복확인 및 변경 api 연결

--- frontend-main/src/app/apis/memberApi.ts
+++ frontend-main/src/app/apis/memberApi.ts
@@ -1,6 +1,8 @@
+// src/app/api/memberApi.ts
 import config from "@/config";
 
-export const checkNickNameExists = async (
+export const checkNicknameExists = async (
+  accessToken: string,
   nickname: string,
 ): Promise<boolean> => {
   const response = await fetch(
@@ -9,15 +11,44 @@ export const checkNickNameExists = async (
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
       },
     },
   );
+
   console.log(response.ok);
+
   if (response.ok) {
-    return true;
+    return false; // 중복없으면 false (사용 가능)
   } else if (response.status === 400) {
-    return false;
+    return true; // 중복이면 true(사용 불가)
   } else {
     throw new Error(`서버 오류: ${response.statusText}`);
+  }
+};
+
+export const changeNicknameSave = async (
+  accessToken: string,
+  newNickname: string,
+): Promise<void> => {
+  try {
+    const response = await fetch(
+      `${config.API_BASE_URL}api/auth/members/nickname`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ nickname: newNickname }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`서버 오류: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("닉네임 변경 중 오류 발생:", error);
+    throw error;
   }
 };
