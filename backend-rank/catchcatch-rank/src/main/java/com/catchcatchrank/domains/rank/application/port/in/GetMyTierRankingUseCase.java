aa28db784c3919790c032edf763a183315db1874
홍경현 <hyeon21owo@gmail.com>
2024-10-06 01:56:15 +0900

    Feat: 내 랭킹을 포함하지 않는 티어 랭킹 조회 use case

--- backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/domains/rank/application/port/in/GetMyTierRankingUseCase.java
+++ backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/domains/rank/application/port/in/GetMyTierRankingUseCase.java
@@ -1,7 +1,9 @@
 package com.catchcatchrank.domains.rank.application.port.in;
 
 import com.catchcatchrank.domains.rank.domain.MyTierRanking;
+import com.catchcatchrank.domains.rank.domain.TierRanking;
 
 public interface GetMyTierRankingUseCase {
 	MyTierRanking getMyTierRanking(String nickname, Integer start);
+	TierRanking getTierRanking(String nickname, Integer start);
 }
