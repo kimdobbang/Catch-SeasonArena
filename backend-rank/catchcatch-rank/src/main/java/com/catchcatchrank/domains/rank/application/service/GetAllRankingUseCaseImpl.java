e74e66e041b0f192a478b755919aaffca3296f1f
홍경현 <hyeon21owo@gmail.com>
2024-10-06 14:18:52 +0900

    Feat: 전체 랭킹 조회 use case

---
+++ backend-rank/catchcatch-rank/src/main/java/com/catchcatchrank/domains/rank/application/service/GetAllRankingUseCaseImpl.java
@@ -0,0 +1,75 @@
+package com.catchcatchrank.domains.rank.application.service;
+
+import com.catchcatchrank.domains.member.appclication.port.GetMemberByNickNamePort;
+import com.catchcatchrank.domains.member.domain.Member;
+import com.catchcatchrank.domains.rank.application.port.in.GetAllRankingUseCase;
+import com.catchcatchrank.domains.rank.application.port.out.GetAllRankPort;
+import com.catchcatchrank.domains.rank.application.port.out.GetMeRankPort;
+import com.catchcatchrank.domains.rank.application.port.out.GetUserTierPort;
+import com.catchcatchrank.domains.rank.domain.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j(topic = "rank")
+public class GetAllRankingUseCaseImpl implements GetAllRankingUseCase {
+
+    private final GetUserTierPort getUserTierPort;
+    private final GetAllRankPort getAllRankPort;
+    private final GetMeRankPort getMeRankPort;
+    private final GetMemberByNickNamePort getMemberByNickNamePort;
+
+    @Value("${rank.limit:5}")
+    private Integer limit;
+
+    @Override
+    public MyAllRanking getMyAllRanking(String nickname, Integer page) {
+        Integer start = page * limit;
+        log.info("BE-RANK :  start {}", start);
+        String tier = getUserTierPort.getUserTier(nickname);
+        log.info("BE-RANK : tier {}", tier);
+        Set<ZSetOperations.TypedTuple<Object>> tierRanksSet = getAllRankPort.getAllRank(start);
+        List<UserRank> allRanks = tierRank(tierRanksSet, start);
+        MyRank myRank = myRank(tier, nickname);
+        return new MyAllRanking(allRanks, myRank);
+    }
+
+    @Override
+    public AllRanking getAllRanking(Integer page) {
+        Integer start = page * limit;
+        log.info("BE-RANK :  start {}", start);
+        Set<ZSetOperations.TypedTuple<Object>> tierRanksSet = getAllRankPort.getAllRank(start);
+        List<UserRank> allRanks = tierRank(tierRanksSet, start);
+        return new AllRanking(allRanks);
+    }
+
+    private List<UserRank> tierRank(Set<ZSetOperations.TypedTuple<Object>> tierRanksSet, Integer start) {
+        int count = start+1;
+        List<UserRank> ranks = new ArrayList<>();
+        for (ZSetOperations.TypedTuple<Object> tuple : tierRanksSet) {
+            String nickname = tuple.getValue().toString();
+            Integer rate = tuple.getScore().intValue();
+            Member member = getMemberByNickNamePort.getMemberByNickName(nickname);
+            UserRank userRank = UserRank.createUserRank(nickname, member.getAvatar(), count++, rate);
+            ranks.add(userRank);
+        }
+        return ranks;
+    }
+
+    private MyRank myRank(String tier, String nickname) {
+        Integer getMyRank = getMeRankPort.getTierOfUserRaking(tier, nickname) + 1;
+        Integer getMyRate = getMeRankPort.getUserRate(tier, nickname);
+
+        return MyRank.createMyRank(tier, nickname, getMyRank, getMyRank, getMyRate);
+    }
+}
