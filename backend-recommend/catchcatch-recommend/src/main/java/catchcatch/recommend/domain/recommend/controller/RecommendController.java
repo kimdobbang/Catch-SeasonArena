cd765bbf53839fc4244f5d5d689db3e9b6406d9f
홍경현 <hyeon21owo@gmail.com>
2024-10-07 15:01:15 +0900

    Feat: 매칭 나가기 controller

--- backend-recommend/catchcatch-recommend/src/main/java/catchcatch/recommend/domain/recommend/controller/RecommendController.java
+++ backend-recommend/catchcatch-recommend/src/main/java/catchcatch/recommend/domain/recommend/controller/RecommendController.java
@@ -1,6 +1,7 @@
 package catchcatch.recommend.domain.recommend.controller;
 
 import catchcatch.recommend.domain.recommend.requestdto.EntryRequestDto;
+import catchcatch.recommend.domain.recommend.requestdto.ExitRequestDto;
 import catchcatch.recommend.domain.recommend.service.RecommendService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,5 +30,11 @@ public class RecommendController {
 
         log.info("BACK/MATCHING - entry player time: {}, rating: {}, day: {}", formattedDate, requestDto.rating(), dayOfWeek);
         recommendService.entryPlayer(requestDto);
+    }
+
+    @MessageMapping("/exit")
+    public void exitPlayer(ExitRequestDto requestDto) {
+        log.info("BACK-RECOMMEND/ exit player: {}", requestDto);
+        recommendService.exitPlayerByNickname(requestDto);
     }
 }
