e986bac68cb00735a6f707ee5369a24536aaf472
고승희 <bookshelf1234@ewhain.net>
2024-10-07 13:13:42 +0900

    Feat: 아이템 카드 버튼 추가

--- frontend-main/src/shared/components/entities/inventory/inventory-item-card.tsx
+++ frontend-main/src/shared/components/entities/inventory/inventory-item-card.tsx
@@ -1,9 +1,11 @@
+import { useLocation } from "react-router-dom";
 import {
   Body1Text,
   Caption1Text,
   CircleTag,
   ItemTypeTag,
   AutumnItemImage,
+  Body2Text,
 } from "@atoms/index";
 import { Line } from "@ui/index";
 import { Item, ItemGrade, ItemType, getDurability } from "@/app/types/common";
@@ -11,13 +13,20 @@ import { Item, ItemGrade, ItemType, getDurability } from "@/app/types/common";
 interface InventoryItemCardProps {
   item: Item;
   onClose: () => void;
+  onCombinationClick?: () => void;
 }
 
 export const InventoryItemCard = ({
   item,
   onClose,
 }: InventoryItemCardProps) => {
-  const { itemId, name, type, grade, durability } = item;
+  // 경로에 따라 행동 유형 결정 (인벤토리/합성)
+  const location = useLocation();
+  const behavior = location.pathname.includes("/combination")
+    ? "combination"
+    : "inventory";
+
+  const { itemId, name, type, grade, durability, skill } = item;
   const maxDurability = getDurability(grade as ItemGrade);
 
   return (
@@ -30,28 +39,55 @@ export const InventoryItemCard = ({
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex flex-col justify-center w-full h-full bg-white rounded-md">
+          {/* 원형 태그 */}
           <div className="h-[5%] w-full justify-end flex p-2 mt-2">
             <CircleTag
               grade={grade as ItemGrade}
               className="bg-catch-gray-300"
             />
           </div>
-          <div className="h-[75%] w-full flex flex-col justify-center gap-2 items-center">
+          {/* 이미지, 아이템 이름, 내구도 */}
+          <div className="h-[65%] w-full flex flex-col justify-center gap-2 items-center">
             <div className="w-[100px] h-[100px] rounded-xl">
               <AutumnItemImage itemId={itemId} />
             </div>
-            <Body1Text>인벤토리 아이템 카드로 수정예정</Body1Text>
             <Body1Text className="!text-catch-gray-300">{name}</Body1Text>
             <Line className="bg-catch-gray-200" />
             <Caption1Text>
               {durability}/{maxDurability} hp
             </Caption1Text>
           </div>
-          <div className="h-[20%] w-full flex items-center justify-center flex-col gap-1">
+          {/* 아이템타입, 스킬 */}
+          <div className="h-[15%] w-full flex items-center justify-center flex-col gap-1">
             <ItemTypeTag color="gray" type={type as ItemType} />
+            <Caption1Text>{skill}</Caption1Text>
           </div>
-          {/* 아이템 설명 */}
           {/* 장착해제 토글 */}
+          <div className="h-[15%] w-full flex items-center justify-center flex-col gap-1">
+            <div className="flex flex-row items-center justify-around w-full h-full">
+              {/* 인벤토리 */}
+              {behavior === "inventory" && (
+                <button>
+                  <Body2Text className="font-bold text-catch-gray-500">
+                    장착
+                  </Body2Text>
+                </button>
+              )}
+              {/* 합성 */}
+              {behavior === "combination" && (
+                <button>
+                  <Body2Text className="font-bold text-catch-gray-500">
+                    추가
+                  </Body2Text>
+                </button>
+              )}
+              <button>
+                <Body2Text className="font-bold text-catch-system-color-error">
+                  삭제
+                </Body2Text>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
