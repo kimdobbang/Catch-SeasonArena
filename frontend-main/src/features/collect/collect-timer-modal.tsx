8615ba14b1175a6b311ea13cb589e48e38af3632
고승희 <bookshelf1234@ewhain.net>
2024-10-10 16:19:56 +0900

    Feat: 인벤토리, 메인에서 수집후 1분동안 다시 수집버튼 클릭 불가

--- frontend-main/src/features/collect/collect-timer-modal.tsx
+++ frontend-main/src/features/collect/collect-timer-modal.tsx
@@ -5,6 +5,14 @@ import { AutumnItemImage } from "@atoms/index";
 
 export const CollectTimerModal = ({ onClose }: { onClose: () => void }) => {
   const { name, itemId } = useSelector((state: RootState) => state.success);
+  // timeSlice 상태에서 collectTime 가져오기
+  const collectTime = useSelector((state: RootState) => state.time.collectTime);
+
+  // collectTime이 0이면 모달을 열지 않음
+  if (collectTime === 0) {
+    onClose();
+    return null; // 모달을 렌더링하지 않음
+  }
 
   return (
     <div
@@ -15,7 +23,7 @@ export const CollectTimerModal = ({ onClose }: { onClose: () => void }) => {
         className={`relative w-[280px] rounded-lg h-[260px] bg-gradient-to-b bg-white`}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="w-full h-full flex flex-col gap-2 justify-center items-center">
+        <div className="flex flex-col items-center justify-center w-full h-full gap-2">
           <Body1Text className="font-medium">
             방금 수집한 <b>{name}</b>
           </Body1Text>
