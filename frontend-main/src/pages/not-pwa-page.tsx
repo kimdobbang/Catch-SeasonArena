ec5339fc1a24d5d602c4da4a5a651b1449e48f79
kimdoi <kimdoiary@gmail.com>
2024-10-11 12:24:10 +0900

    FE/pwa들어 가세요

--- frontend-main/src/pages/not-pwa-page.tsx
+++ frontend-main/src/pages/not-pwa-page.tsx
@@ -1,33 +1,16 @@
-import { useNavigate } from "react-router-dom";
 import { Copyright } from "@ui/index";
 import { ServiceTitle } from "@ui/index";
 export const NotPwaPage = () => {
-  const navigate = useNavigate();
-
-  const goToMain = () => {
-    navigate("/main");
-  };
-  const goToLogin = () => {
-    navigate("/login");
-  };
-
   return (
     <div className="flex flex-col items-center justify-around h-screen bg-catch-sub-100">
       <div className="flex flex-col items-center">
         <ServiceTitle />
       </div>
-      <div className="text-center font-pretendard text-title">PWA설치안내</div>
-      <div>모바일 환경에서 아이폰: 홈화면 추가</div>
-      <div>
-        <button onClick={goToMain} className="bg-catch-main-400">
-          Main으로
-        </button>
+      <div className="text-center font-pretendard text-title">
+        PWA앱을 설치해주세요
       </div>
-      <div>
-        <button onClick={goToLogin} className="bg-catch-tier-silver">
-          로그인으로
-        </button>
-      </div>
+      <div>아이폰: 홈화면 추가</div>
+      <div>갤럭시 아이폰: 홈화면 다운로드</div>
 
       <Copyright />
     </div>
