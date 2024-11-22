e14bab87651af3875f316cc4abd423829aa54d25
kimdoi <kimdoiary@gmail.com>
2024-11-22 16:03:05 +0900

    Refactor: widget분리(헤더, 레이아웃, 전체배경, 페이지네이션)

---
+++ frontend-main/src/shared/widgets/index.ts
@@ -0,0 +1,6 @@
+// widget 공개 API
+
+export { Background }from "@widgets/background";
+export { Header } from "@widgets//header";
+export { Layout, HeaderLayout } from "@widgets//layouts";
+export { NumberPagination } from "@widgets//number-pagination";
